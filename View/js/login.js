8657d2ff1778a321a98c632c42579e7a61d913b2
evandroMSchmitz <evandroschmitz2010@hotmail.com>
2016-03-07 11:11:23 -0300

    Login com Ajax e JavaScript
    Isso é só pra ser um pequeno teste, será ajustado quando a base de dados
    for formulada adequadamente.

---
+++ View/js/login.js
@@ -0,0 +1,22 @@
+$(document).ready(function (e) {
+    $("#enter").click(function (e) {
+        var vLogin = $("#login").val(), vPassword = $("#password").val();
+        
+        if (vLogin.length === 0) {
+            alert("Atenção\nO campo de Login está vazio");
+            //$("#login").focus();
+        } else if (vPassword.length === 0) {
+            alert("Atenção\nO campo de Senha está vazio");
+            //$("#password").focus();
+        } else {
+            $.post("/MinhaCreche/controller/user.php", {login: vLogin, senha: vPassword}, function(retorno) {
+                if (retorno) {
+                    alert("Passou Miseraverl");
+                } else {
+                    //$("#login").focus;
+                    $("#erro").show();
+				}
+            });
+        }
+    });
+});
