b290a4de9a993858f5730455ee5170926babf72a
evandroMSchmitz <evandroschmitz2010@hotmail.com>
2016-03-09 18:07:22 -0300

    Menu include
    O menu já é um include, nomes de arquivos foram modificados.

---
+++ MinhaCreche/design/home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Minha Creche</title>
+        <link rel="stylesheet" href="css/estilo_menu3.css" type="text/css">
+    </head>
+    <body>
+        <?php include 'menu_principal.php' ?>
+    </body>
+</html>
