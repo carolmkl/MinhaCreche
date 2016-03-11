dc1c9770113d9da60d7808c251e176880987bb38
evandroMSchmitz <evandroschmitz2010@hotmail.com>
2016-03-11 17:23:44 -0300

    Views basiconas
    View da Creche e pagina de home OK. As outras estão muito básicas, MUITO
    básicas.

---
+++ design/avisos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Minha Creche</title>
+        <link rel="stylesheet" href="css/estilo_menu3.css" type="text/css">
+        <link rel="stylesheet" href="css/estilo_conteudo.css" type="text/css">
+        <link rel="stylesheet" href="css/estilo_imagem.css" type="text/css">
+
+    </head>
+    <body>
+        <div>
+            <?php include 'menu_principal.php' ?>
+
+            <div class="conteiner">
+                <h1 class="space">Avisos</h1>
+                <div class="space">
+                    
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ design/menu_principal.php
@@ -0,0 +1,15 @@
+<div id="menu">
+    <span><a href="home.php"><img id="logo" src="imagens/logo2_0_100.png" alt="Minha Creche"></a></span>
+          <ul class="box">
+              <li><a href="creche.php">Creche</a></li>
+              <li><a href="funcionarios.php">Funcionários</a></li>
+              <!-- <li><a href="#">Turmas</a></li>-->
+              <li><a href="responsaveis.php">Responsáveis</a></li>
+              <li><a href="criancas.php">Crianças</a></li>
+              <!-- <li><a href="#">Medicamentos</a></li> -->
+              <li><a href="avisos.php">Avisos</a></li>
+              <!-- <li><a href="#">Recessos</a></li>
+              <li><a href="#">Atividades</a></li> -->
+              <li><a href="login.html">Sair</a></li>
+          </ul>
+</div>
