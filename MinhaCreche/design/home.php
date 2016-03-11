2452a3ea117bd17a8a725ca4d09b201f9295d0fd
evandroMSchmitz <evandroschmitz2010@hotmail.com>
2016-03-11 16:31:54 -0300

    Home e Dados da Creche
    A pagina de home e dados da creche estão feitas, não são responsivas,
    mas contudo tem um desempenho razoável. O alinhamento esquerda menu e
    centro-direita conteúdo também está aceitável.

--- MinhaCreche/design/home.php
+++ MinhaCreche/design/home.php
@@ -4,8 +4,15 @@
         <meta charset="utf-8">
         <title>Minha Creche</title>
         <link rel="stylesheet" href="css/estilo_menu3.css" type="text/css">
+        <link rel="stylesheet" href="css/estilo_conteudo.css" type="text/css">
+        <link rel="stylesheet" href="css/estilo_imagem.css" type="text/css">
+
     </head>
     <body>
         <?php include 'menu_principal.php' ?>
+        
+        <div class="conteiner">
+            <div id="img_center"><img id="logo" src="imagens/logo2_0.png" alt="Minha Creche"></div>
+        </div>
     </body>
 </html>
--- MinhaCreche/design/menu_principal.php
+++ MinhaCreche/design/menu_principal.php
@@ -1,15 +1,15 @@
 <div id="menu">
-            <span><img id="logo" src="imagens/logo2_0_100.png" alt="Minha Creche"></span>
-              <ul class="box">
-                  <li><a href="#">Creche</a></li>
-                  <li><a href="#">Funcionários</a></li>
-                  <!-- <li><a href="#">Turmas</a></li>-->
-                  <li><a href="#">Responsáveis</a></li>
-                  <li><a href="#">Crianças</a></li>
-                  <!-- <li><a href="#">Medicamentos</a></li> -->
-                  <li><a href="#">Avisos</a></li>
-                  <!-- <li><a href="#">Recessos</a></li>
-                  <li><a href="#">Atividades</a></li> -->
-                  <li><a href="#">Sair</a></li>
-              </ul>
-        </div>
+    <span><a href="home.php"><img id="logo" src="imagens/logo2_0_100.png" alt="Minha Creche"></a></span>
+          <ul class="box">
+              <li><a href="creche.php">Creche</a></li>
+          <li><a href="#">Funcionários</a></li>
+              <!-- <li><a href="#">Turmas</a></li>-->
+              <li><a href="#">Responsáveis</a></li>
+              <li><a href="#">Crianças</a></li>
+              <!-- <li><a href="#">Medicamentos</a></li> -->
+              <li><a href="#">Avisos</a></li>
+              <!-- <li><a href="#">Recessos</a></li>
+              <li><a href="#">Atividades</a></li> -->
+              <li><a href="#">Sair</a></li>
+          </ul>
+</div>
