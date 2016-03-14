9b2184f799a9ae1db8f5d47b79420024440181d6
evandroMSchmitz <evandroschmitz2010@hotmail.com>
2016-03-14 07:49:38 -0300

    Reorganização
    Reorganizando os arquivos

--- MinhaCreche/design/menu_principal.php
+++ MinhaCreche/design/menu_principal.php
@@ -2,14 +2,14 @@
     <span><a href="home.php"><img id="logo" src="imagens/logo2_0_100.png" alt="Minha Creche"></a></span>
           <ul class="box">
               <li><a href="creche.php">Creche</a></li>
-          <li><a href="#">Funcionários</a></li>
+              <li><a href="funcionarios.php">Funcionários</a></li>
               <!-- <li><a href="#">Turmas</a></li>-->
-              <li><a href="#">Responsáveis</a></li>
-              <li><a href="#">Crianças</a></li>
+              <li><a href="responsaveis.php">Responsáveis</a></li>
+              <li><a href="criancas.php">Crianças</a></li>
               <!-- <li><a href="#">Medicamentos</a></li> -->
-              <li><a href="#">Avisos</a></li>
+              <li><a href="avisos.php">Avisos</a></li>
               <!-- <li><a href="#">Recessos</a></li>
               <li><a href="#">Atividades</a></li> -->
-              <li><a href="#">Sair</a></li>
+              <li><a href="login.html">Sair</a></li>
           </ul>
 </div>
