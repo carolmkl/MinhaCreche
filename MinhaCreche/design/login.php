18e640c5d3ce713267b4a3c11d8d74773e310fb3
carolmkl <carolmkl@gmail.com>
2016-03-21 09:00:40 -0300

    Ajuste no login

--- MinhaCreche/design/login.php
+++ MinhaCreche/design/login.php
@@ -1,16 +1,16 @@
 <?php
 
-if(!isset($_POST["Submit"])){
+if(!isset($_POST['Submit'])){
     include "index.html";
 }else{
     include "conexaodb.php";
 
-    $usuario = $_REQUEST["mclogin"];
-    $senha = $_REQUEST["mcpassword"];
+    $usuario = mysqli_escape_string($conn,$_REQUEST['mclogin']);
+    $senha = mysqli_escape_string($conn,$_REQUEST['mcpassword']);
 
 
     /*Aqui verificamos se o usuario e senha digitados no formulário existem e estão corretos*/
-    $sql = "SELECT 'ok' FROM usuario WHERE login='".mysqli_escape_string($conn,$usuario)."' and senha='".mysqli_escape_string($conn,$senha)."'";
+    $sql = "SELECT u.nivel,pf.nome,f.id_creche, f.id_Funcionario, r.id_responsavel FROM pessoafisica pf inner join usuario u on u.id_PessoaFisica = pf.id_pessoaFisica left join funcionario f on pf.id_pessoaFisica = f.id_pessoaFisica left join responsavel r on pf.id_pessoaFisica = r.id_pessoaFisica where u.login = '$usuario' and u.senha = '$senha';";
     $result = mysqli_query($conn,$sql);
 
     if($result === FALSE) { 
@@ -24,10 +24,17 @@ if(!isset($_POST["Submit"])){
     } 
     else 
     {
-       /*Apos verificado ele grava o usuario e senha no vetor $_session[]*/
+      $row = mysqli_fetch_assoc($result);
+       //grava os dados da sessão
        session_start();
        $_SESSION['mclogin']=$usuario;
        $_SESSION['mcpassword']=$senha;
+       $_SESSION['mc_user_nome']=$row['nome'];
+       $_SESSION['mc_user_nivel']=$row['nivel'];
+       $_SESSION['mc_funcionario_id']=$row['id_Funcionario'];
+       $_SESSION['mc_creche_id']=$row['id_creche'];
+       $_SESSION['mc_responsável_id']=$row['id_responsavel'];
+
        header("Location: home.php");
     }
 
