4e74be31c0738abdac695bd1b1d32bfb91894cde
evandroMSchmitz <evandroschmitz2010@hotmail.com>
2016-04-11 15:34:01 -0300

    Validações com regexp
    Validações feitas em JavaScript, só falta uma ultima validação no
    servido pra fazer as coisas ficarem nos trinks

--- MinhaCreche/design/funcionario_form.php
+++ MinhaCreche/design/funcionario_form.php
@@ -7,10 +7,12 @@
         <link rel="stylesheet" href="css/estilo_conteudo.css" type="text/css">
         <link rel="stylesheet" href="css/estilo_imagem.css" type="text/css">
         <script src="js/callPage.js"></script>
+        <script src="js/vadilacoes.js"></script>
         <?php include 'import.php' ?>
         <?php include '../code/valida_user.php' ?>
         
         <script type="text/javascript">
+            
             $(document).ready(function (e) {
                 var vIdFunc = Number($("#idFuncionario").val());
                 $("#submit").click(function (e) {
@@ -34,43 +36,122 @@
                         vIdPessoa = Number($("#idPessoa").val());
                         
                     var vDestino;
+                    var verificaSenha = true;
                     if(vIdFunc != 0 ){
                         vDestino = "../code/funcionarioUpdate.php";
+                        verificaSenha = false;
                     } else {
                         vDestino = "../code/funcionarioInsert.php";
                     }
+                    
+                    var erros = [];
+                    
+                    if(!validaNome(vNome)){
+                        erros[erros.length] = "Nome inválido. Somente letras e números de tamanho mínimo 3."
+                        $("#nome").css("border-color", "red");
+                    }
+                    if(!validaCpf(vCpf)){
+                        erros[erros.length] = "CPF inválido. Somente os 11 números."
+                        $("#cpf").css("border-color", "red");
+                    }
+                    if(!validaRg(vRg)){
+                        erros[erros.length] = "RG inválido. Somente os 5 caracteres, sem pontos."
+                        $("#rg").css("border-color", "red");
+                    }
+                    if(vEmail != "" && !validaEmail(vEmail)){
+                        erros[erros.length] = "Email inválido."
+                        $("#email").css("border-color", "red");
+                    }
+                    if(!validaTelefone(vTelefone)){
+                        erros[erros.length] = "Telefone inválido. Formato (##) ####### ou (##) ########."
+                        $("#telefone").css("border-color", "red");
+                    }
+                    if(vCelular != "" && !validaTelefone(vCelular)){
+                        erros[erros.length] = "Celular inválido. Formato (##) ####### ou (##) ########."
+                        $("#celular").css("border-color", "red");
+                    }
+                    if(vDtNascimento == ""){
+                        erros[erros.length] = "Data de nascimento inválida. Informe uma data."
+                        $("#dtNascimento").css("border-color", "red");
+                    }
+                    if(vGenero == ""){
+                        erros[erros.length] = "Selecione o Gênero."
+                    }
+                    if(!validaNome(vLogradouro)){
+                        erros[erros.length] = "Logradouro inválido. Somente letras e números de tamanho mínimo 3."
+                        $("#logradouro").css("border-color", "red");
+                    }
+                    if(!validaNumero(vNumero)){
+                        erros[erros.length] = "Número inválido."
+                        $("#numero").css("border-color", "red");
+                    }
+                    if(!validaNome(vBairro)){
+                        erros[erros.length] = "Bairro inválido. Somente letras e números de tamanho mínimo 3."
+                        $("#bairro").css("border-color", "red");
+                    }
+                    if(!validaNome(vCidade)){
+                        erros[erros.length] = "Cidade inválida. Somente letras e números de tamanho mínimo 3."
+                        $("#cidade").css("border-color", "red");
+                    }
+                    if(!validaEstado(vEstado)){
+                        erros[erros.length] = "Estado inválido. Somente letras e números de tamanho mínimo 3."
+                        $("#estado").css("border-color", "red");
+                    }
+                    if(vCargo == ""){
+                        erros[erros.length] = "Selecione o Cargo."
+                    }
+                    if(vObservacao != "" && !validaNome(vObservacao)){
+                        erros[erros.length] = "Observação inválida. Somente letras e números de tamanho mínimo 3."
+                        $("#observacao").css("border-color", "red");
+                    }
+                    if(!validaLogin(vLogin)){
+                        erros[erros.length] = "Login inválido. Somente letras, números e '_' de tamanho mínimo 3."
+                        $("#login").css("border-color", "red");
+                    }
+                    if((verificaSenha || vSenha != "") && !validaSenha(vSenha)){
+                        erros[erros.length] = "Senha inválida. Somente letras números e os caracteres !, @, #, $, %, ^, &, *, (), _ de tamanho mínimo 6."
+                        $("#senha").css("border-color", "red");
+                    }
 
-                    $.post(vDestino, {
-                        id_funcionario : vIdFunc,
-                        id_pessoafisica : vIdPessoa,
-                        nome : vNome,
-                        cpf : vCpf,
-                        rg : vRg,
-                        email : vEmail,
-                        telefone : vTelefone,
-                        celular : vCelular,
-                        dtNascimento : vDtNascimento,
-                        genero : vGenero,
-                        logradouro : vLogradouro,
-                        numero : vNumero,
-                        bairro : vBairro,
-                        cidade : vCidade,
-                        estado : vEstado,
-                        observacao : vObservacao,
-                        cargo : vCargo,
-                        login: vLogin,
-                        senha: vSenha
-                    }, function (retorno) {
-                        console.log(retorno);
-                        var dadosRetorno = JSON.parse(retorno);
-                        console.log(dadosRetorno);
-                        alert(retorno);
-                        if (dadosRetorno == true) {
-                            window.location.replace("funcionarios.php");
-                        } else {
-                            alert("Ocorreu um erro inesperado ao realizar a operação!");
+                    if(erros.length == 0){
+                            $.post(vDestino, {
+                            id_funcionario : vIdFunc,
+                            id_pessoafisica : vIdPessoa,
+                            nome : vNome,
+                            cpf : vCpf,
+                            rg : vRg,
+                            email : vEmail,
+                            telefone : vTelefone,
+                            celular : vCelular,
+                            dtNascimento : vDtNascimento,
+                            genero : vGenero,
+                            logradouro : vLogradouro,
+                            numero : vNumero,
+                            bairro : vBairro,
+                            cidade : vCidade,
+                            estado : vEstado,
+                            observacao : vObservacao,
+                            cargo : vCargo,
+                            login: vLogin,
+                            senha: vSenha
+                        }, function (retorno) {
+                            console.log(retorno);
+                            var dadosRetorno = JSON.parse(retorno);
+                            console.log(dadosRetorno);
+                            if (dadosRetorno == true) {
+                                window.location.replace("funcionarios.php");
+                            } else {
+                                alert("Ocorreu um erro inesperado ao realizar a operação!");
+                            }
+                        });
+                    } else {
+                        var msg = "Corrija estes erros:";
+                        for (var i = 0; i<erros.length; i++) {
+                            msg += "\n" + erros[i];
                         }
-                    });
+                        alert(msg);
+                    }
+                    
                 });
                 
                 if(vIdFunc != 0){
@@ -129,29 +210,29 @@
 
                         <fieldset class="fieldset_border">
                             <legend>Acesso ao Sistema</legend>
-                            <label class="text" for="name">Login</label>
+                            <label class="text" for="name">Login<sup>*</sup></label>
                             <input class="width" type="text" value="" size="20" id="login" ng-model="login" required><br>
-                            <label class="text" for="name">Senha</label>
+                            <label class="text" for="name">Senha<sup>*</sup></label>
                             <input class="width" type="password" value="" size="20" id="senha" ng-model="senha" required><br>
                         </fieldset><br><br>
 
                         <fieldset class="fieldset_border">
                             <legend>Dados Pessoais</legend>
-                            <label class="text" for="name">Nome</label>
+                            <label class="text" for="name">Nome<sup>*</sup></label>
                             <input class="text_big" type="text" id="nome" ng-model="nome"> 
-                            <label class="text" for="rg">RG</label>
+                            <label class="text" for="rg">RG<sup>*</sup></label>
                             <input class="text_small" type="text" id="rg" ng-model="rg" >
-                            <label class="text" for="cpf">CPF</label>
+                            <label class="text" for="cpf">CPF<sup>*</sup></label>
                             <input class="text_small" type="text" id="cpf" ng-model="cpf" ><br>
                             <label class="text" for="mail">E-mail</label>
                             <input class="text_bigger" type="email" id="email" ng-model="email" ><br>
-                            <label class="text" for="telefone">Telefone</label>
+                            <label class="text" for="telefone">Telefone<sup>*</sup></label>
                             <input type="tel" id="telefone" ng-model="telefone" >
                             <label class="text" for="celular">celular</label>
                             <input type="tel" id="celular" ng-model="celular" ><br>
-                            <label class="text" for="nascimento">Data de Nascimento</label>
+                            <label class="text" for="nascimento">Data de Nascimento<sup>*</sup></label>
                             <input type="date" id="dtNascimento" ng-model="dtNascimento" >
-                            <label class="text" for="celular">Gênero</label>
+                            <label class="text" for="celular">Gênero<sup>*</sup></label>
                             <select placeholder="Selecione" class="dropdown" for="genero" id="genero" ng-model="genero" >
                                     <option value="">Selecione</option>
                                     <option value="M">Masculino</option>
@@ -164,15 +245,15 @@
 
                         <fieldset class="fieldset_border">
                             <legend>Endereço</legend>
-                            <label class="text" for="logradouro">Logradouro</label>
+                            <label class="text" for="logradouro">Logradouro<sup>*</sup></label>
                             <input class="text_bigger" type="text" id="logradouro" ng-model="logradouro" > 
-                            <label class="text" for="num">Numero</label>
+                            <label class="text" for="num">Numero<sup>*</sup></label>
                             <input class="text_small" type="number" id="numero" ng-model="numero" ><br>
-                            <label class="text" for="bairro">Bairro</label>
+                            <label class="text" for="bairro">Bairro<sup>*</sup></label>
                             <input class="text_big" type="text" id="bairro" ng-model="bairro" > 
-                            <label class="text" for="city">Cidade</label>
+                            <label class="text" for="city">Cidade<sup>*</sup></label>
                             <input type="text" id="cidade" ng-model="cidade" >
-                            <label class="text" for="state">Estado</label>
+                            <label class="text" for="state">Estado<sup>*</sup></label>
                             <input class="text_smaller" type="text" id="estado" ng-model="estado" ><br>
                             <label class="text" for="obs">Observação</label> 
                             <input class="text_bigger" type="text" id="observacao" ng-model="observacao" ><br>
@@ -180,7 +261,7 @@
 
                         <br>
                         <br>
-                        <label class="text" for="cargo">Perfil</label>
+                        <label class="text" for="cargo">Perfil<sup>*</sup></label>
                         <select class="dropdown" for="cargo" id="cargo" ng-model="cargo" >
                                 <option value="">Selecione</option>
                                 <option value="Diretor">Diretor</option>
@@ -189,6 +270,7 @@
                         </select>
 
                         <br>
+                        <p id="obrigatorio"><sup>*</sup> Campos obrigatórios</p>
                         <br>
 
                         <p> 
---
+++ MinhaCreche/design/js/vadilacoes.js
@@ -0,0 +1,44 @@
+function validaNome(texto) {
+    var teste = /^[A-Za-z0-9 ]{3,}$/;
+    return teste.test(texto);
+}
+
+function validaEstado(texto) {
+    var teste = /^[A-Za-z0-9 ]{2,}$/;
+    return teste.test(texto);
+}
+
+function validaCpf(texto) {
+    var teste = /^[0-9]{11}$/;
+    return teste.test(texto);
+}
+
+function validaRg(texto) {
+    var teste = /^[A-Za-z0-9]{7}$/;
+    return teste.test(texto);
+}
+
+function validaEmail(texto) {
+    var teste = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+    return teste.test(texto);
+}
+
+function validaTelefone(texto) {
+    var teste = /^([0-9][0-9])\s[0-9]{8,9}$/;
+    return teste.test(texto);
+}
+
+function validaNumero(texto) {
+    var teste = /^[0-9]{1,}$/;
+    return teste.test(texto);
+}
+
+function validaLogin(texto) {
+    var teste = /^\w{3,20}$/;
+    return teste.test(texto);
+}
+
+function validaSenha(texto) {
+    var teste = /^[A-Za-z0-9!@#$%^&*()_]{6,20}$/;
+    return teste.test(texto);
+}
